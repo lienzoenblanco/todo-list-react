f406a77011168f370a67be1dbf7da118065525f5
lienzoenblanco <raquelgarciatrives@gmail.com>
2022-04-06 17:26:11 +0000

    second commit

--- src/js/component/Todo.jsx
+++ src/js/component/Todo.jsx
@@ -1,24 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Input, Checkbox, Select } from "antd";
 
 const Todo = () => {
+	const priorities = ["urgente", "importante", "normal"];
+
+	const [taskDoneCounter, setTaskDoneCounter] = useState(0);
+
+	const [taskNormalCounter, setTaskNormalCounter] = useState(0);
+	const [taskUrgenteCounter, setTaskUrgenteCounter] = useState(0);
+	const [taskImportanteCounter, setTaskImportanteCounter] = useState(0);
+
 	const [todoList, setTodoList] = useState([]);
-	const [task, setTask] = useState("");
+	const [task, setTask] = useState({
+		name: "",
+		priority: "importante",
+		done: false,
+	});
+	const [priorityValue, setPriorityValue] = useState();
+
+	useEffect(() => {
+		calculateNormalTask();
+		calculateUrgenteTask();
+		calculateImportanteTask();
+	}, [todoList]);
+
+	const calculateDoneTask = () => {
+		const todoListDone = todoList.filter((item) => {
+			return item.done == true;
+		});
+		setTaskDoneCounter(todoListDone.length);
+	};
+
+	const calculateNormalTask = () => {
+		const todoListNormal = todoList.filter((item) => {
+			return item.priority == "normal";
+		});
+		setTaskNormalCounter(todoListNormal.length);
+	};
+
+	const calculateUrgenteTask = () => {
+		const todoListUrgente = todoList.filter((item) => {
+			return item.priority == "urgente";
+		});
+		setTaskUrgenteCounter(todoListUrgente.length);
+	};
+
+	const calculateImportanteTask = () => {
+		const todoListImportante = todoList.filter((item) => {
+			return item.priority == "importante";
+		});
+		setTaskImportanteCounter(todoListImportante.length);
+	};
 
 	const handleChange = (event) => {
-		setTask(event.target.value);
+		setTask({ ...task, name: event.target.value });
 	};
 
 	const addNewTask = (event) => {
 		event.preventDefault();
-		if (task.trim().length !== 0) {
+		if (task.name.trim().length !== 0) {
 			setTodoList([...todoList, task]);
 		}
 	};
 
-	function onChange(e) {
-		console.log(`checked = ${e.target.checked}`);
+	function onChange(e, taskIndex) {
+		todoList[taskIndex].done = e.target.checked;
+		setTodoList(todoList);
+		calculateDoneTask();
 	}
 
 	return (
@@ -30,10 +79,11 @@ const Todo = () => {
 						name="new-task"
 						type="text"
 						placeholder="add a task"
+						autoComplete="off"
 					/>
 				</span>
 				<span className="select">
-					{/* <Select defaultValue={normal} onChange={handleChange}>
+					{/* <Select defaultValue="normal">
 						<Option value="urgente">Urgente</Option>
 						<Option value="importante">Importante</Option>
 						<Option value="normal">Normal</Option>
@@ -41,18 +91,56 @@ const Todo = () => {
 				</span>
 			</form>
 			<ul>
-				{todoList.map((taskPrint) => {
+				{todoList.map((taskPrint, index) => {
 					return (
-						<li>
+						<li key={index}>
 							<span className="check">
-								<Checkbox onChange={onChange}></Checkbox>
+								<Checkbox
+									onChange={(event) => {
+										onChange(event, index);
+									}}></Checkbox>
 							</span>
-							{taskPrint}
+							{taskPrint.name}
+							--
+							{taskPrint.done == true}
 						</li>
 					);
 				})}
 			</ul>
-			<div className="counters"></div>
+			<div className="counters">
+				<ul className="list-group">
+					<li className="list-group-item d-flex justify-content-between align-items-center">
+						Urgente
+						<span className="badge bg-primary rounded-pill">
+							{taskUrgenteCounter}
+						</span>
+					</li>
+					<li className="list-group-item d-flex justify-content-between align-items-center">
+						Importante
+						<span className="badge bg-primary rounded-pill">
+							{taskImportanteCounter}
+						</span>
+					</li>
+					<li className="list-group-item d-flex justify-content-between align-items-center">
+						Normal
+						<span className="badge bg-primary rounded-pill">
+							{taskNormalCounter}
+						</span>
+					</li>
+					<li className="list-group-item d-flex justify-content-between align-items-center">
+						Hechas
+						<span className="badge bg-primary rounded-pill">
+							{taskDoneCounter}
+						</span>
+					</li>
+					<li className="list-group-item d-flex justify-content-between align-items-center">
+						Total
+						<span className="badge bg-primary rounded-pill">
+							{todoList.length}
+						</span>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
